3eebf7229af8e0647d5183c5b106fda6b3942f04
Maksimov25 <dmaksimov_89@mail.ru>
2024-10-04 11:35:05 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header />
+        <nuxt :key="$route.fullPath" />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+
+export default {
+    name: "Default",
+    components: {
+        Header,
+        Footer
+    }
+};
+</script>
+
+<style lang="scss">
+.page-main {
+    position: relative;
+    z-index: $zMain;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'site',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/image_gl.jpg' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  styleResources: {
+    scss: ['~/assets/scss/variables.scss', '~/assets/scss/_z-index.scss']
+  },
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/style-resources',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
